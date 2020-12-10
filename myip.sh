2cf7b7f08abd1fab3b2045fb35bc5dde34638237
foo2rama <66143330+foo2rama@users.noreply.github.com>
2020-12-09 17:20:15 -0800

    Create myip.sh

---
+++ myip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# Phil Maurer 8.17.2020 V 1.0
+# phil@m4urer.com
+# This is a script to grab the IP and external IP of the machine in use.
+
+cho -e "\e[38;5;82mLocal IP\e[0m"
+hostname -I | awk '{print $1}'
+echo " "
+echo -e "\e[38;5;82mExternal IP\e[0m"
+dig +short myip.opendns.com @resolver1.opendns.com
+echo " "

b69d0ba7beb8c9a614d24c517a2d138d0d6c8387
foo2rama <66143330+foo2rama@users.noreply.github.com>
2021-07-13 20:53:33 -0700

    Create randompicker.sh

---
+++ randompicker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Simplescript to pick names from a list.
+# Phil Maurer
+# phil@m4urer.com
+# v 1.0 - 7.13.2021
+#
+# Instructions: The names.txt file needs to have one name on each
+#               line.  Of course you can put any data if you want
+#
+# note : you will have to change the absolute pathing depending on
+#        where you place it.  If you decide to run it via alias you
+#        really need the absolute path to the name file
+#        ( I highly recommend running it as an alias)
+
+clear
+echo " "
+echo " "
+echo -e "and the winner is ...\e[92m"
+echo " "
+shuf -n 1 /home/sysadmin/Desktop/picker/names.txt
+echo -e " \033[0m"
+echo " "

ed8c07ddfd553487e4f10ea36614e2a49e5a5aa9
foo2rama <66143330+foo2rama@users.noreply.github.com>
2022-11-28 18:17:24 -0800

    Create simpledockerup.sh

---
+++ simpledockerup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#  phil@m4urer.com
+#  version .9 6/10/2021 - initial
+#  version 1.0 6/10/2021 - rewrite
+
+echo " "
+echo " "
+echo "Starting Kali Container"
+echo " "
+docker start friendly_mclaren
+echo " "
+echo "Connecting to Kali Container"
+echo " "
+echo " "
+docker attach friendly_mclaren
